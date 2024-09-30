b996ee8ff44583243d2c7f6189d2745bcc4b1818
Tomoya Otake <tomoya.ton@gmail.com>
2024-09-30 23:20:45 +0900

    Apply biome format

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,11 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 
-const app = new Hono()
+const app = new Hono();
 app.use("*", prettyJSON());
 
-app.get('/', (c) => {
-  return c.text("Hello Hono!")
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
