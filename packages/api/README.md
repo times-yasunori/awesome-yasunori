fe9d6a68b96803d5a32a2878b0df834bd580ee9e
Tomoya Otake <tomoya.ton@gmail.com>
2024-09-30 17:50:37 +0900

    Run npm create hono@latest and initial setup

---
+++ packages/api/README.md
@@ -0,0 +1,10 @@
+# yasunori APIs
+
+```txt
+npm install
+npm run dev
+```
+
+```txt
+npm run deploy
+```
---
+++ packages/api/src/index.ts
@@ -0,0 +1,11 @@
+import { Hono } from 'hono'
+import { prettyJSON } from "hono/pretty-json";
+
+const app = new Hono()
+app.use("*", prettyJSON());
+
+app.get('/', (c) => {
+  return c.text("Hello Hono!")
+})
+
+export default app
