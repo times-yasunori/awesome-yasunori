a7c5c0421f77562c1467b87818d91afdc330e3dd
Tomoya Otake <tomoya.ton@gmail.com>
2024-09-30 23:24:57 +0900

    Add cors and poweredBy plugin and improve return value

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,20 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
+import { cors } from "hono/cors";
+import { poweredBy } from "hono/powered-by";
+import { getParsedAwesomeYasunori } from "./get-parsed-awesome-yasunori";
 
 const app = new Hono();
+app.use("*", poweredBy());
 app.use("*", prettyJSON());
+app.use("*", cors());
+
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.json({
+    message:
+      "Here is yasunori APIs. <https://github.com/takeokunn/awesome-yasunori/packages/api>",
+  });
 });
 
 export default app;
