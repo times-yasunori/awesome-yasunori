94f1f2639af9e347e85cee4c5acb162bf831cedd
takeokunn <bararararatty@gmail.com>
2024-10-01 09:23:35 +0900

    Fix lint

--- flake.nix
+++ flake.nix
@@ -1,52 +1,29 @@
 {
   description = "A curated list of awesome yasunori";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs =
-    inputs@{
-      self,
-      systems,
-      nixpkgs,
-      treefmt-nix,
-      flake-parts,
-    }:
+  outputs = inputs@{ self, systems, nixpkgs, treefmt-nix, flake-parts, }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ treefmt-nix.flakeModule ];
-      systems = import inputs.systems;
+      systems = import systems;
 
-      perSystem =
-        {
-          config,
-          pkgs,
-          system,
-          ...
-        }:
-        let
-          stdenv = pkgs.stdenv;
-          yasunori = pkgs.emptyDirectory;
-        in
-        rec {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixfmt.enable = true;
-            };
-
-            settings.formatter = { };
+      perSystem = { pkgs, ... }: {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            taplo.enable = true;
           };
-
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              nil
-            ];
-          };
-
-          packages.default = yasunori;
         };
+
+        devShells.default = pkgs.mkShell { packages = with pkgs; [ nil ]; };
+
+        packages.default = pkgs.emptyDirectory;
+      };
     };
 }
