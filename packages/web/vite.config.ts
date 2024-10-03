b67b267842cecc4cb45febc4b2ef6122eb95ee48
Tomoya Otake <tomoya.ton@gmail.com>
2024-10-03 22:10:50 +0900

    Use unstable_singleFetch

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     remixCloudflareDevProxy(),
     remix({
       future: {
+        unstable_singleFetch: true,
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
