91c91c03c4392045b8c04d36fc2cd6f7629bc1df
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-03 17:50:49 +0100

    feat(packages/web): pnpm monorepoを使ってweb側からapiのappの型を取得

--- packages/web/app/shared/yasunori-api.ts
+++ packages/web/app/shared/yasunori-api.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
-import type { AppType } from "../../../api/src/index.js";
+import type { AppType } from "@awesome-yasunori/api/src/index.js";
 
 export const yasunoriAPIClient = hc<AppType>("https://api.yasunori.dev");
