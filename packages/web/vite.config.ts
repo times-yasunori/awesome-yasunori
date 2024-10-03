d90c9d8a6c539caf95bb108b72583f6f422e4cc1
Tomoya Otake <tomoya.ton@gmail.com>
2024-10-03 21:47:45 +0900

    Setup SSR

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,11 +1,14 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    remixCloudflareDevProxy(),
     remix({
-      ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
