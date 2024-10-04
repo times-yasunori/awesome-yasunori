e590a4d9f33f53276fd108e5f3ea3d4acbc92e21
Tomoya Otake <tomoya.ton@gmail.com>
2024-10-05 03:23:56 +0900

    Add ogp meta to entry

--- packages/web/app/routes/entries.$id/route.tsx
+++ packages/web/app/routes/entries.$id/route.tsx
@@ -1,9 +1,34 @@
 import { Button, Flex, Group, Stack, rem } from "@mantine/core";
+import type { MetaFunction } from "@remix-run/cloudflare";
 import { useLoaderData, useNavigate } from "@remix-run/react";
 import { AwesomeYasunoriCard } from "../../components/awesome-yasunori-card";
 import { type EntryLoader, entryLoader } from "./loader";
 
 export const loader = entryLoader;
+
+export const meta: MetaFunction<EntryLoader> = ({ data, location }) => {
+  const title = data?.title ?? "Awesome Yasunori";
+  const description = data?.content ?? "Awesome Yasurunori Content";
+  const url = `https://awesome.yasunori.dev${location.pathname}`;
+  const date = data?.date
+    ? new Date(data.date).toISOString()
+    : new Date().toISOString();
+  return [
+    { title },
+    { name: "description", content: description },
+    { property: "og:url", content: url },
+    { property: "og:site_name", content: "Awesome Yasunori" },
+    { property: "og:title", content: title },
+    { property: "og:description", content: description },
+    { property: "og:locale", content: "ja" },
+    { property: "og:type", content: "article" },
+    { property: "article:section", content: "posts" },
+    { property: "article:published_time", content: date },
+    { property: "article:modified_time", content: date },
+    // TODO
+    // { property: 'og:image', content: "url" },
+  ];
+};
 
 export default function Entry() {
   const entry = useLoaderData<EntryLoader>();
