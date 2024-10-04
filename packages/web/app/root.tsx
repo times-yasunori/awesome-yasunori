f6986a13a09e44e3c73137276a6b44401bd916a8
Tomoya Otake <tomoya.ton@gmail.com>
2024-10-05 01:03:35 +0900

    Hide Navbar if data is undefined

--- packages/web/app/root.tsx
+++ packages/web/app/root.tsx
@@ -43,6 +43,7 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const data = useRouteLoaderData<IndexLoader>("routes/_index");
+  const isShowNavbar = !!data;
   const isMobile = useMediaQuery(`(max-width: ${em(767)})`);
   const pinned = useHeadroom({ fixedAt: 120 });
   const [opened, { toggle, close }] = useDisclosure();
@@ -60,11 +61,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <MantineProvider forceColorScheme="dark">
           <AppShell
             header={{ height: 60, collapsed: !pinned }}
-            navbar={{
-              width: 300,
-              breakpoint: "sm",
-              collapsed: { mobile: !opened },
-            }}
+            navbar={
+              isShowNavbar
+                ? {
+                    width: 300,
+                    breakpoint: "sm",
+                    collapsed: { mobile: !opened },
+                  }
+                : undefined
+            }
             padding="md"
           >
             <AppShell.Header p="md">
@@ -110,30 +115,32 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 </Group>
               </Group>
             </AppShell.Header>
-            <AppShell.Navbar p="md">
-              <AppShell.Section grow component={ScrollArea}>
-                {data?.map((d) => (
-                  <NavLink
-                    key={d.id}
-                    onClick={() => {
-                      navigate(`#${d.id}`, { replace: true });
-                      // モバイルのときは移動後にサイドバーを閉じる
-                      if (isMobile) {
-                        close();
+            {isShowNavbar && (
+              <AppShell.Navbar p="md">
+                <AppShell.Section grow component={ScrollArea}>
+                  {data?.map((d) => (
+                    <NavLink
+                      key={d.id}
+                      onClick={() => {
+                        navigate(`#${d.id}`, { replace: true });
+                        // モバイルのときは移動後にサイドバーを閉じる
+                        if (isMobile) {
+                          close();
+                        }
+                      }}
+                      label={
+                        <Group gap="xs">
+                          <Text>{`${d.title}`}</Text>
+                          <Text size="xs" c="dimmed">
+                            {`#${d.id}`}
+                          </Text>
+                        </Group>
                       }
-                    }}
-                    label={
-                      <Group gap="xs">
-                        <Text>{`${d.title}`}</Text>
-                        <Text size="xs" c="dimmed">
-                          {`#${d.id}`}
-                        </Text>
-                      </Group>
-                    }
-                  />
-                ))}
-              </AppShell.Section>
-            </AppShell.Navbar>
+                    />
+                  ))}
+                </AppShell.Section>
+              </AppShell.Navbar>
+            )}
             <AppShell.Main pt={`calc(${rem(60)} + var(--mantine-spacing-md))`}>
               {children}
             </AppShell.Main>
