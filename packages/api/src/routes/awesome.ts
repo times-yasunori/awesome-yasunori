ad7385a04aa0078acbb52cd79e33524eea195c25
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-04 15:51:21 +0100

    feat(packages/web): create random endpoint

--- packages/api/src/routes/awesome.ts
+++ packages/api/src/routes/awesome.ts
@@ -1,18 +1,31 @@
 import { Hono } from "hono";
+import randomItem from "random-item";
 import { getParsedAwesomeYasunori } from "../get-parsed-awesome-yasunori";
 
 const app = new Hono();
 
 const parsedAwesomeYasunori = getParsedAwesomeYasunori();
 
-export const route = app.get("/", async (c) => {
-  if (!parsedAwesomeYasunori.success) {
-    return c.json(
-      {
-        errors: parsedAwesomeYasunori.issues,
-      },
-      400,
-    );
-  }
-  return c.json(parsedAwesomeYasunori?.output.yasunori);
-});
+export const route = app
+  .get("/", async (c) => {
+    if (!parsedAwesomeYasunori.success) {
+      return c.json(
+        {
+          errors: parsedAwesomeYasunori.issues,
+        },
+        400,
+      );
+    }
+    return c.json(parsedAwesomeYasunori?.output.yasunori);
+  })
+  .get("/random", async (c) => {
+    if (!parsedAwesomeYasunori.success) {
+      return c.json(
+        {
+          errors: parsedAwesomeYasunori.issues,
+        },
+        400,
+      );
+    }
+    return c.json(randomItem(parsedAwesomeYasunori?.output.yasunori));
+  });
