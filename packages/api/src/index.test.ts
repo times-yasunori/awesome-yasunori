22afd761bbcb4b1ef615b84c13223c6b65a5aa2f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-04 15:49:23 +0100

    feat(packages/web): add test for random endpoint

--- packages/api/src/index.test.ts
+++ packages/api/src/index.test.ts
@@ -37,3 +37,25 @@ describe("Test GET /awesome", () => {
     });
   });
 });
+
+describe("Test GET /awesome/random", () => {
+  test("Should return 200 response", async () => {
+    const res = await app.request("http://localhost/awesome/random");
+    expect(res.status).toBe(200);
+  });
+
+  test("Should return entries response", async () => {
+    const res = await app.request("http://localhost/awesome/random");
+    const parsed = await res.json<Array<unknown>>();
+    expect(parsed.at(-1)).toStrictEqual({
+      id: 1,
+      at: "vim-jp radioお便り",
+      content:
+        "tomoyaさん、ありすえさんこんにちは。\nはじめまして、yasunoriの母です。\n\nyasunoriがソフトウェアエンジニアを志してから様子がおかしくなってしまいました。\n家ですれ違う時「Vim....Vim....」という独り言をずっと唱えていたり、部屋からは「設定させていただきありがとうございます!!」という大声が聞こえてきたり、\n「会合があるから東京に行ってくる、帰りは遅くなる」と言い残して出て行き、帰ってくると満面の笑みで「Vimはいいぞ」と一言言って自室に篭るようになりました。\n\ntomoyaさんありすえさんもVimコミュニティの人達だと伺いましたが、息子の身に一体何が起きてしまったのか教えていただけると幸いです。\n",
+      date: "2024-06-25",
+      meta: "",
+      senpan: "takeokunn",
+      title: "yasunoriの母",
+    });
+  });
+});
