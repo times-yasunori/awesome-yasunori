d75108a774db219a6ee92ed65ed72bb76d7a4a25
Tomoya Otake <tomoya.ton@gmail.com>
2024-10-06 00:54:12 +0900

    Implement GET /awesome/:id API

--- packages/api/src/index.test.ts
+++ packages/api/src/index.test.ts
@@ -67,3 +67,39 @@ describe("Test GET /awesome/random", () => {
     });
   });
 });
+
+describe("Test GET /awesome/random", () => {
+  test("Should return 200 response", async () => {
+    const res = await app.request("/awesome/1");
+    expect(res.status).toBe(200);
+  });
+
+  test("Should return entries response", async () => {
+    const res = await app.request("/awesome/1");
+    const parsed = await res.json<unknown>();
+    expect(parsed).toStrictEqual({
+      id: 1,
+      at: "vim-jp radioお便り",
+      content:
+        "tomoyaさん、ありすえさんこんにちは。\nはじめまして、yasunoriの母です。\n\nyasunoriがソフトウェアエンジニアを志してから様子がおかしくなってしまいました。\n家ですれ違う時「Vim....Vim....」という独り言をずっと唱えていたり、部屋からは「設定させていただきありがとうございます!!」という大声が聞こえてきたり、\n「会合があるから東京に行ってくる、帰りは遅くなる」と言い残して出て行き、帰ってくると満面の笑みで「Vimはいいぞ」と一言言って自室に篭るようになりました。\n\ntomoyaさんありすえさんもVimコミュニティの人達だと伺いましたが、息子の身に一体何が起きてしまったのか教えていただけると幸いです。\n",
+      date: "2024-06-25",
+      meta: "",
+      senpan: "takeokunn",
+      title: "yasunoriの母",
+    });
+  });
+
+  test("Should return 404 error response if entry not found", async () => {
+    const res = await app.request("/awesome/0");
+    expect(res.status).toBe(404);
+    expect(await res.json()).toStrictEqual({ errors: ["not found"] });
+  });
+
+  test("Should return 404 error response if params is not number", async () => {
+    const res = await app.request("/awesome/id");
+    expect(res.status).toBe(404);
+    expect(await res.json()).toMatchObject({
+      errors: [{ type: "safe_integer" }],
+    });
+  });
+});
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,9 +2,9 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { poweredBy } from "hono/powered-by";
 import { prettyJSON } from "hono/pretty-json";
-
 import randomItem from "random-item";
 import { getParsedAwesomeYasunori } from "./get-parsed-awesome-yasunori";
+import { getParsedRequestParams } from "./get-parsed-request-params";
 
 const parsedAwesomeYasunori = getParsedAwesomeYasunori();
 
@@ -41,6 +41,32 @@ const route = app
       );
     }
     return c.json(randomItem(parsedAwesomeYasunori?.output.yasunori));
+  })
+  .get("/awesome/:id", async (c) => {
+    const parsedParams = getParsedRequestParams(c.req.param());
+    if (!parsedParams.success) {
+      return c.json(
+        {
+          errors: parsedParams.issues,
+        },
+        404,
+      );
+    }
+    if (!parsedAwesomeYasunori.success) {
+      return c.json(
+        {
+          errors: parsedAwesomeYasunori.issues,
+        },
+        400,
+      );
+    }
+    const entry = parsedAwesomeYasunori.output.yasunori.find(
+      (y) => y.id === parsedParams.output.id,
+    );
+    if (!entry) {
+      return c.json({ errors: ["not found"] }, 404);
+    }
+    return c.json(entry);
   });
 
 export default app;
