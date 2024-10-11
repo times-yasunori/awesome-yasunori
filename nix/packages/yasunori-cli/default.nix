4a010c0a78cfa981efbb43f6b8ea5155f6868ed2
kuuote <znmxodq1@gmail.com>
2024-10-11 10:33:40 +0900

    refactor(nix): use writeNuBin

--- nix/packages/yasunori-cli/default.nix
+++ nix/packages/yasunori-cli/default.nix
@@ -2,6 +2,6 @@
   writers,
   awesome-yasunori,
 }:
-writers.writeNu "/bin/yasunori-cli" ''
+writers.writeNuBin "yasunori-cli" ''
   open ${awesome-yasunori}/yasunori.toml | get yasunori | get content | get (random dice --sides (($in | length) - 1) | get 0)
 ''
