0248857effd804b70037969927f386593cec6393
Naoya Yamashita <conao3@gmail.com>
2024-10-12 13:34:59 +0900

    use :fig alias

---
+++ packages/cljs-yasunori-block/.dir-locals.el
@@ -0,0 +1,3 @@
+((clojure-mode . ((cider-clojure-cli-aliases . ":fig")
+                  (cider-default-cljs-repl . figwheel-main)
+                  (cider-figwheel-main-default-options . "dev"))))
