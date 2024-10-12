905e8c83eb2fac34e8f2c61adc85264da00ad2e7
Naoya Yamashita <conao3@gmail.com>
2024-10-12 13:50:59 +0900

    define app

---
+++ packages/cljs-yasunori-block/src/yasunori_block/app.cljs
@@ -0,0 +1,5 @@
+(ns yasunori-block.app)
+
+(defn app []
+  [:div
+   [:h1 "hello"]])
--- packages/cljs-yasunori-block/src/yasunori_block/core.cljs
+++ packages/cljs-yasunori-block/src/yasunori_block/core.cljs
@@ -1,8 +1,14 @@
-(ns yasunori-block.core)
+(ns ^:figwheel-hooks yasunori-block.core
+  (:require
+   [reagent.dom :as rdom]
+   [yasunori-block.app :as y.app]))
 
 (enable-console-print!)
 
-(defn hello []
-  "hello world")
+(defn mount-app-element []
+  (rdom/render [y.app/app] (js/document.getElementById "app")))
 
-(println (hello))
+(mount-app-element)
+
+(defn ^:after-load on-reload []
+  (mount-app-element))
