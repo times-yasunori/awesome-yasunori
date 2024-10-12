3a4c0f1f186e22b2feb7d36fe088201fae162c99
Naoya Yamashita <conao3@gmail.com>
2024-10-12 17:56:56 +0900

    add ball calc logic

--- packages/cljs-yasunori-block/src/yasunori_block/app.cljs
+++ packages/cljs-yasunori-block/src/yasunori_block/app.cljs
@@ -10,37 +10,106 @@
   (println "title")
   [:h1 "cljs-yasunori-block"])
 
+(defn initial-state [width height]
+  {:ball {:cx (/ width 2) :cy height :radius 5 :dx -4 :dy -5}
+   :paddle {:cx (/ width 2) :cy height :width 200 :height 20}
+   :blocks []})
+
 (defn Main []
   (println "main")
   (let [canvas (atom nil)
         width 1000
         height 500
         is-running (reagent/atom false)
-        state (reagent/atom {:ball {:x 0 :y 0 :dx 0 :dy 0}
-                             :paddle {:width 0 :cx 0 :cy 0}
-                             :blocks []})
+        state (reagent/atom (initial-state width height))
         ticks (reagent/atom 0)
         seconds-elapsed (reagent/atom 0)
         seconds-timeout-id (reagent/atom nil)]
-    (letfn [(draw-paddle [])
+    (letfn [(debounce []
+              (= 0 (mod @ticks 50)))
+
+            (draw-centered-rect [ctx {:keys [:cx :cy :width :height]}]
+              (let [hw (/ width 2)
+                    hh (/ height 2)]
+                (. ctx fillRect (- cx hw) (- cy hh) width height)))
+
+            (draw-centered-circle [ctx {:keys [:cx :cy :radius]}]
+              (. ctx beginPath)
+              (. ctx arc cx cy radius 0 (* js/Math.PI 2))
+              (. ctx fill)
+              (. ctx closePath))
+
+            (draw-ball [ctx]
+              (let [{:keys [:ball]} @state]
+                (set! (.-fillStyle ctx) "0095DD")
+                (draw-centered-circle ctx ball)))
+
+            (draw-paddle [ctx]
+              (let [{:keys [:paddle]} @state]
+                (set! (.-fillStyle ctx) "#0095DD")
+                (draw-centered-rect ctx paddle)))
+
+            (draw-1 [ctx]
+              ;; 初期化
+              (. ctx clearRect 0 0 width height)
+
+              ;; 現在の状態を描く
+              (draw-paddle ctx)
+              (draw-ball ctx)
+
+              ;; 左右の壁との衝突 -> dxを符号反転
+              (let [{:keys [:cx :radius :dx]} (:ball @state)]
+                (when (or (< width (+ cx dx radius))
+                          (< (+ cx dx (- radius)) 0))
+                  (swap! state update-in [:ball :dx] -)))
+
+              ;; 上の壁との衝突 -> dyを符号反転
+              (let [{:keys [:cy :radius :dy]} (:ball @state)]
+                (when (< (+ cy dy (- radius)) 0)
+                  (swap! state update-in [:ball :dy] -)))
+
+              ;; 下の壁との衝突
+              ;;  - パドル: dyを符号反転
+              ;;  - それ以外: ゲームオーバー
+              (let [{:keys [:cx :cy :radius :dy]} (:ball @state)
+                    {pcx :cx pwidth :width} (:paddle @state)
+                    phw (/ pwidth 2) ; paddle-harf-width
+                    plx (- pcx phw)  ; paddle-left-x
+                    prx (+ pcx phw)  ; paddle-right-x
+                    ]
+                (when (< height (+ cy dy radius))
+                  (if (and (< plx cx) (< cx prx))
+                    (swap! state update-in [:ball :dy] -)
+                    (end-game))))
+
+              ;; ボールを動かす
+              (swap! state update :ball #(-> %
+                                             (update :cx + (:dx %))
+                                             (update :cy + (:dy %)))))
+
             (draw []
-              (swap! ticks inc)
-              (when-let [canvas @canvas]
-                (let [ctx (. canvas getContext "2d")]
-                  (. ctx clearRect 0 0 width height)
-                  (draw-paddle)))
+              (when (debounce)
+                (println @state))
+              (when @canvas
+                (draw-1 (. @canvas getContext "2d")))
               (when @is-running
+                (swap! ticks inc)
                 (reagent.core/next-tick draw)))
+
             (start-game []
               (reset! is-running true)
               (reset! seconds-timeout-id
                       (js/setInterval #(swap! seconds-elapsed inc) 1000))
+              (reset! state (initial-state width height))
+              (reset! ticks 0)
+              (reset! seconds-elapsed 0)
               (draw))
+
             (end-game []
               (reset! is-running false)
               (when @seconds-timeout-id
-                (js/clearInterval @seconds-timeout-id))
-              (reset! seconds-timeout-id nil))]
+                (js/clearInterval @seconds-timeout-id)
+                (reset! seconds-timeout-id nil)))]
       (fn []
         [:div
          [:div
@@ -49,6 +118,8 @@
            "Ticks: " (print-str @ticks)]]
          [:div
           [:canvas {:ref #(reset! canvas %)
+                    :width width
+                    :height height
                     :style {:width (str width "px")
                             :height (str height "x")
                             :border "1px solid black"}}]]
