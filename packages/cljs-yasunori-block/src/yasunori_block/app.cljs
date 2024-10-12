eadf5ce65a22efc87e6102c173a26e07b8fb7532
Naoya Yamashita <conao3@gmail.com>
2024-10-12 15:32:12 +0900

    add timer

--- packages/cljs-yasunori-block/src/yasunori_block/app.cljs
+++ packages/cljs-yasunori-block/src/yasunori_block/app.cljs
@@ -2,6 +2,10 @@
   (:require
    [reagent.core :as reagent]))
 
+(defn Timer [{:keys [:seconds-elapsed]}]
+  [:div
+   [:p "Seconds Elapsed: " seconds-elapsed "s"]])
+
 (defn Title []
   (println "title")
   [:h1 "cljs-yasunori-block"])
@@ -9,20 +13,35 @@
 (defn Main []
   (println "main")
   (let [canvas (atom nil)
-        is-running (reagent/atom false)]
+        is-running (reagent/atom false)
+        seconds-elapsed (reagent/atom 0)
+        seconds-timeout-id (reagent/atom nil)
+        start-game (fn []
+                     (reset! is-running true)
+                     (reset! seconds-timeout-id
+                             (js/setInterval #(swap! seconds-elapsed inc) 1000)))
+        end-game (fn []
+                   (reset! is-running false)
+                   (when @seconds-timeout-id
+                     (js/clearInterval @seconds-timeout-id))
+                   (reset! seconds-timeout-id nil))]
     (fn []
       (println @is-running)
       [:div
+       [:div
+        [Timer {:seconds-elapsed @seconds-elapsed}]]
        [:div
         [:canvas {:ref #(reset! canvas %)
                   :style {:width "1000px"
                           :height "500px"
                           :border "1px solid black"}}]]
        [:div {:style {:text-align "center"}}
-        [:button {:on-click #(swap! is-running not)}
-         (if @is-running
-           "Restart YASUNORI"
-           "Start YASUNORI")]]])))
+        [:button {:on-click #(if (not @is-running)
+                               (start-game)
+                               (end-game))}
+         (if (not @is-running)
+           "Start YASUNORI"
+           "Stop YASUNORI")]]])))
 
 (defn App []
   [:div {:style {:max-width "1000px"}}
