96b3ad2afb6c1ecfc4baad466088183dbabcfd34
Naoya Yamashita <conao3@gmail.com>
2024-10-12 16:10:34 +0900

    add event loop

--- packages/cljs-yasunori-block/src/yasunori_block/app.cljs
+++ packages/cljs-yasunori-block/src/yasunori_block/app.cljs
@@ -13,35 +13,52 @@
 (defn Main []
   (println "main")
   (let [canvas (atom nil)
+        width 1000
+        height 500
         is-running (reagent/atom false)
+        state (reagent/atom {:ball {:x 0 :y 0 :dx 0 :dy 0}
+                             :paddle {:width 0 :cx 0 :cy 0}
+                             :blocks []})
+        ticks (reagent/atom 0)
         seconds-elapsed (reagent/atom 0)
-        seconds-timeout-id (reagent/atom nil)
-        start-game (fn []
-                     (reset! is-running true)
-                     (reset! seconds-timeout-id
-                             (js/setInterval #(swap! seconds-elapsed inc) 1000)))
-        end-game (fn []
-                   (reset! is-running false)
-                   (when @seconds-timeout-id
-                     (js/clearInterval @seconds-timeout-id))
-                   (reset! seconds-timeout-id nil))]
-    (fn []
-      (println @is-running)
-      [:div
-       [:div
-        [Timer {:seconds-elapsed @seconds-elapsed}]]
-       [:div
-        [:canvas {:ref #(reset! canvas %)
-                  :style {:width "1000px"
-                          :height "500px"
-                          :border "1px solid black"}}]]
-       [:div {:style {:text-align "center"}}
-        [:button {:on-click #(if (not @is-running)
-                               (start-game)
-                               (end-game))}
-         (if (not @is-running)
-           "Start YASUNORI"
-           "Stop YASUNORI")]]])))
+        seconds-timeout-id (reagent/atom nil)]
+    (letfn [(draw-paddle [])
+            (draw []
+              (swap! ticks inc)
+              (when-let [canvas @canvas]
+                (let [ctx (. canvas getContext "2d")]
+                  (. ctx clearRect 0 0 width height)
+                  (draw-paddle)))
+              (when @is-running
+                (reagent.core/next-tick draw)))
+            (start-game []
+              (reset! is-running true)
+              (reset! seconds-timeout-id
+                      (js/setInterval #(swap! seconds-elapsed inc) 1000))
+              (draw))
+            (end-game []
+              (reset! is-running false)
+              (when @seconds-timeout-id
+                (js/clearInterval @seconds-timeout-id))
+              (reset! seconds-timeout-id nil))]
+      (fn []
+        [:div
+         [:div
+          [Timer {:seconds-elapsed @seconds-elapsed}]
+          [:div
+           "Ticks: " (print-str @ticks)]]
+         [:div
+          [:canvas {:ref #(reset! canvas %)
+                    :style {:width (str width "px")
+                            :height (str height "x")
+                            :border "1px solid black"}}]]
+         [:div {:style {:text-align "center"}}
+          [:button {:on-click #(if (not @is-running)
+                                 (start-game)
+                                 (end-game))}
+           (if (not @is-running)
+             "Start YASUNORI"
+             "Stop YASUNORI")]]]))))
 
 (defn App []
   [:div {:style {:max-width "1000px"}}
