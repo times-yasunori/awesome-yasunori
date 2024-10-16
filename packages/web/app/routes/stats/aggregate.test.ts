d866e419c1c16381e4d9b7f5638c8d37b69a0f4c
Tomoya Otake <tomoya.ton@gmail.com>
2024-10-17 00:24:20 +0900

    Carete senpanAggregate

--- packages/web/app/routes/stats/aggregate.test.ts
+++ packages/web/app/routes/stats/aggregate.test.ts
@@ -1,4 +1,4 @@
-import { monthlyPostsAggregate } from "./aggregate";
+import { monthlyPostsAggregate, senpanAggregate } from "./aggregate";
 
 describe("monthlyPostsAggregate", () => {
   test("return aggregateed data", () => {
@@ -58,6 +58,63 @@ describe("monthlyPostsAggregate", () => {
         month: 10,
         yearMonth: "2024-10",
         amount: 2,
+      },
+    ]);
+  });
+});
+
+describe("senpanAggregate", () => {
+  test("return aggregateed data", () => {
+    const source = [
+      {
+        id: 52,
+        title: "特技はyasunori",
+        date: "2024-10-13",
+        at: "vim-jp #times-comamoca",
+        senpan: "comamoca",
+        content: "content",
+        meta: "meta",
+      },
+      {
+        id: 51,
+        title: "最高に「YASUNORI！」ってやつだァァァァァ",
+        date: "2024-10-10",
+        at: "vim-jp #times-yasunori",
+        senpan: "tomoya",
+        content: "content",
+        meta: "meta",
+      },
+      {
+        id: 50,
+        title: "YASUNORYYYYYYYYYYYY！！",
+        date: "2024-09-10",
+        at: "vim-jp #times-yasunori",
+        senpan: "tomoya",
+        content: "content",
+        meta: "meta",
+      },
+      {
+        id: 49,
+        title: "ねぇ、今から消えるよ",
+        date: "2024-08-10",
+        at: "vim-jp #times-yasunori",
+        senpan: "natsukium",
+        content: "content",
+        meta: "meta",
+      },
+    ];
+    expect(senpanAggregate(source)).toStrictEqual([
+      {
+        senpan: "tomoya",
+        amount: 2,
+      },
+      {
+        senpan: "comamoca",
+        amount: 1,
+      },
+      {
+        senpan: "natsukium",
+        amount: 1,
       },
     ]);
   });
