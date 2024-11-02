74d25c437210a1a00f08c96b66ca3c0b5647dd75
Tomoya Otake <tomoya.ton@gmail.com>
2024-11-02 17:02:55 +0900

    Implement yasunori query to index loader

--- packages/web/app/routes/_index/loader.ts
+++ packages/web/app/routes/_index/loader.ts
@@ -1,8 +1,10 @@
 import type { LoaderFunction } from "@remix-run/cloudflare";
 import { fetchAwesomeYasunori } from "~/shared/fetch-awesome-yasunori";
 
-export const indexLoader = (async () => {
-  const data = await fetchAwesomeYasunori();
+export const indexLoader = (async ({ request }) => {
+  const url = new URL(request.url);
+  const yasunori = url.searchParams.get("yasunori") ?? undefined;
+  const data = await fetchAwesomeYasunori(yasunori ? { yasunori } : undefined);
   if (!data) {
     throw new Response(null, { status: 404 });
   }
