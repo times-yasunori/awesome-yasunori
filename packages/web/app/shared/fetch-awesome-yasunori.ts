6c67cc8646dfc4c7a5d569be0d62c0c38080745a
Tomoya Otake <tomoya.ton@gmail.com>
2024-11-02 17:02:12 +0900

    Add query argument to fetchAwesomeYasunori

--- packages/web/app/shared/fetch-awesome-yasunori.ts
+++ packages/web/app/shared/fetch-awesome-yasunori.ts
@@ -1,7 +1,11 @@
 import { yasunoriApiClient } from "./yasunori-api";
 
-export async function fetchAwesomeYasunori() {
-  const res = await yasunoriApiClient.awesome.$get();
+export async function fetchAwesomeYasunori(
+  query?: { yasunori: string } | undefined,
+) {
+  const res = await yasunoriApiClient.awesome.$get(
+    query?.yasunori ? { query: { yasunori: query.yasunori } } : undefined,
+  );
   if (!res.ok) {
     return null;
   }
