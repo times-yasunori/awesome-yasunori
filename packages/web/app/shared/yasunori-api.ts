173d658d063a5dcb3f1b86918969a181291bcc8f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-12-30 11:34:09 +0900

    feat: move api client web -> api

--- packages/web/app/shared/yasunori-api.ts
+++ packages/web/app/shared/yasunori-api.ts
@@ -1,4 +1,1 @@
-import type { ApiRoute } from "@awesome-yasunori/api/src/index.js";
-import { hc } from "hono/client";
-
-export const yasunoriApiClient = hc<ApiRoute>("https://api.yasunori.dev");
+export * from "@awesome-yasunori/api/src/client";
