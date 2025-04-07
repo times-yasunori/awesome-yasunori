32ccf7bf067625142da1575549ae65d74d7491ce
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-07 20:11:57 +0100

    test(mcp): remove it

--- packages/mcp/src/index.test.ts
+++ packages/mcp/src/index.test.ts
@@ -3,116 +3,110 @@ import { InMemoryTransport } from "@modelcontextprotocol/sdk/inMemory.js";
 import { describe, expect, it } from "vitest";
 import { server } from "./index.js";
 
-describe("getAllAwesomeYasunori", () => {
-  it("get all awesome yasunori", async () => {
-    // create client for testing
-    const client = new Client({
-      name: "test client",
-      version: "0.1.0",
-    });
+describe("getAllAwesomeYasunori", async () => {
+  // create client for testing
+  const client = new Client({
+    name: "test client",
+    version: "0.1.0",
+  });
 
-    // create in-memory transport for testing
-    const [clientTransport, serverTransport] =
-      InMemoryTransport.createLinkedPair();
+  // create in-memory transport for testing
+  const [clientTransport, serverTransport] =
+    InMemoryTransport.createLinkedPair();
 
-    // connect client and server
-    await Promise.all([
-      client.connect(clientTransport),
-      server.connect(serverTransport),
-    ]);
+  // connect client and server
+  await Promise.all([
+    client.connect(clientTransport),
+    server.connect(serverTransport),
+  ]);
 
-    // call the tool
-    const result = await client.callTool({
-      name: "getAllAwesomeYasunori",
-      arguments: {},
-    });
-    console.log(result);
+  // call the tool
+  const result = await client.callTool({
+    name: "getAllAwesomeYasunori",
+    arguments: {},
+  });
+  console.log(result);
 
-    // expect the result to be a string
-    expect(result).toEqual({
-      content: [
-        {
-          type: "text",
-          text: expect.stringContaining("all awesome yasunori"),
-        },
-      ],
-    });
+  // expect the result to be a string
+  expect(result).toEqual({
+    content: [
+      {
+        type: "text",
+        text: expect.stringContaining("all awesome yasunori"),
+      },
+    ],
   });
 });
 
-describe("getRandomAwesomeYasunori", () => {
-  it("get a random awesome yasunori", async () => {
-    // create client for testing
-    const client = new Client({
-      name: "test client",
-      version: "0.1.0",
-    });
+describe("getRandomAwesomeYasunori", async () => {
+  // create client for testing
+  const client = new Client({
+    name: "test client",
+    version: "0.1.0",
+  });
 
-    // create in-memory transport for testing
-    const [clientTransport, serverTransport] =
-      InMemoryTransport.createLinkedPair();
+  // create in-memory transport for testing
+  const [clientTransport, serverTransport] =
+    InMemoryTransport.createLinkedPair();
 
-    // connect client and server
-    await Promise.all([
-      client.connect(clientTransport),
-      server.connect(serverTransport),
-    ]);
+  // connect client and server
+  await Promise.all([
+    client.connect(clientTransport),
+    server.connect(serverTransport),
+  ]);
 
-    // call the tool
-    const result = await client.callTool({
-      name: "getRandomAwesomeYasunori",
-      arguments: {},
-    });
-    console.log(result);
+  // call the tool
+  const result = await client.callTool({
+    name: "getRandomAwesomeYasunori",
+    arguments: {},
+  });
+  console.log(result);
 
-    // expect the result to be a string
-    expect(result).toEqual({
-      content: [
-        {
-          type: "text",
-          text: expect.stringContaining("random awesome yasunori"),
-        },
-      ],
-    });
+  // expect the result to be a string
+  expect(result).toEqual({
+    content: [
+      {
+        type: "text",
+        text: expect.stringContaining("random awesome yasunori"),
+      },
+    ],
   });
 });
 
-describe("getAwesomeYasunoriById", () => {
-  it("get awesome yasunori by id", async () => {
-    // create client for testing
-    const client = new Client({
-      name: "test client",
-      version: "0.1.0",
-    });
+describe("getAwesomeYasunoriById", async () => {
+  // create client for testing
+  const client = new Client({
+    name: "test client",
+    version: "0.1.0",
+  });
 
-    // create in-memory transport for testing
-    const [clientTransport, serverTransport] =
-      InMemoryTransport.createLinkedPair();
+  // create in-memory transport for testing
+  const [clientTransport, serverTransport] =
+    InMemoryTransport.createLinkedPair();
 
-    // connect client and server
-    await Promise.all([
-      client.connect(clientTransport),
-      server.connect(serverTransport),
-    ]);
+  // connect client and server
+  await Promise.all([
+    client.connect(clientTransport),
+    server.connect(serverTransport),
+  ]);
 
-    // call the tool
-    const result = await client.callTool({
-      name: "getAwesomeYasunoriById",
-      arguments: { id: 1 },
-    });
+  // call the tool
+  const result = await client.callTool({
+    name: "getAwesomeYasunoriById",
+    arguments: { id: 1 },
+  });
 
-    // expect the result to be a string
-    expect(result).toEqual({
-      content: [
-        {
-          type: "text",
-          text: expect.stringContaining(
-            JSON.stringify(
-              "tomoyaさん、ありすえさんこんにちは。\nはじめまして、yasunoriの母です。\n\nyasunoriがソフトウェアエンジニアを志してから様子がおかしくなってしまいました。\n家ですれ違う時「Vim....Vim....」という独り言をずっと唱えていたり、部屋からは「設定させていただきありがとうございます!!」という大声が聞こえてきたり、\n「会合があるから東京に行ってくる、帰りは遅くなる」と言い残して出て行き、帰ってくると満面の笑みで「Vimはいいぞ」と一言言って自室に篭るようになりました。\n\ntomoyaさんありすえさんもVimコミュニティの人達だと伺いましたが、息子の身に一体何が起きてしまったのか教えていただけると幸いです。\n",
-            ),
+  // expect the result to be a string
+  expect(result).toEqual({
+    content: [
+      {
+        type: "text",
+        text: expect.stringContaining(
+          JSON.stringify(
+            "tomoyaさん、ありすえさんこんにちは。\nはじめまして、yasunoriの母です。\n\nyasunoriがソフトウェアエンジニアを志してから様子がおかしくなってしまいました。\n家ですれ違う時「Vim....Vim....」という独り言をずっと唱えていたり、部屋からは「設定させていただきありがとうございます!!」という大声が聞こえてきたり、\n「会合があるから東京に行ってくる、帰りは遅くなる」と言い残して出て行き、帰ってくると満面の笑みで「Vimはいいぞ」と一言言って自室に篭るようになりました。\n\ntomoyaさんありすえさんもVimコミュニティの人達だと伺いましたが、息子の身に一体何が起きてしまったのか教えていただけると幸いです。\n",
           ),
-        },
-      ],
-    });
+        ),
+      },
+    ],
   });
 });
