48423c6f06e1dd41ce7f3bbd5907bf279f9a829f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-07 15:00:42 +0100

    feat(mcp): build with unbuild

---
+++ packages/mcp/build.config.ts
@@ -0,0 +1,17 @@
+import * as child_process from "node:child_process";
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  rollup: {
+    output: {
+      banner: "#!/usr/bin/env node\n",
+    },
+  },
+  hooks: {
+    "build:done": (ctx) => {
+      const { bin } = ctx.pkg;
+      // make the dist/index.js executable
+      child_process.execSync(`chmod +x ./${bin}`);
+    },
+  },
+});
