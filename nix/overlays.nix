8db9a5b2ed8714de5a207ffd48d65aa11a82e49d
kuuote <36663503+kuuote@users.noreply.github.com>
2025-09-10 17:50:15 +0900

    Nix flake update (#203)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/c621e8422220273271f52058f618c94e405bb0f5?narHash=sha256-hIshGgKZCgWh6AYJpJmRgFdR3WUbkY04o82X05xqQiY%3D' (2025-04-01)
      → 'github:hercules-ci/flake-parts/4524271976b625a4a605beefd893f270620fd751?narHash=sha256-%2BuWLQZccFHwqpGqr2Yt5VsW/PbeJVTn9Dk6SHWhNRPw%3D' (2025-09-01)
    • Updated input 'flake-parts/nixpkgs-lib':
        'github:nix-community/nixpkgs.lib/e4822aea2a6d1cdd36653c134cacfd64c97ff4fa?narHash=sha256-b1EdN3cULCqtorQ4QeWgLMrd5ZGOjLSLemfa00heasc%3D' (2025-03-30)
      → 'github:nix-community/nixpkgs.lib/a73b9c743612e4244d865a2fdee11865283c04e6?narHash=sha256-x2rJ%2BOvzq0sCMpgfgGaaqgBSwY%2BLST%2BWbZ6TytnT9Rk%3D' (2025-08-10)
    • Updated input 'git-hooks-nix':
        'github:cachix/git-hooks.nix/dcf5072734cb576d2b0c59b2ac44f5050b5eac82?narHash=sha256-DwOTp7nvfi8mRfuL1escHDXabVXFGT1VlPD1JHrtrco%3D' (2025-03-22)
      → 'github:cachix/git-hooks.nix/ab82ab08d6bf74085bd328de2a8722c12d97bd9d?narHash=sha256-E9spYi9lxm2f1zWQLQ7xQt8Xs2nWgr1T4QM7ZjLFphM%3D' (2025-09-07)
    • Updated input 'git-hooks-nix/flake-compat':
        'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33?narHash=sha256-kvjfFW7WAETZlt09AgDn1MrtKzP7t90Vf7vypd3OL1U%3D' (2023-10-04)
      → 'github:edolstra/flake-compat/9100a0f413b0c601e0533d1d94ffd501ce2e7885?narHash=sha256-CIVLLkVgvHYbgI2UpXvIIBJ12HWgX%2BfjA8Xf8PUmqCY%3D' (2025-05-12)
    • Updated input 'git-hooks-nix/nixpkgs':
        'github:NixOS/nixpkgs/a04d33c0c3f1a59a2c1cb0c6e34cd24500e5a1dc?narHash=sha256-8AKquNnnSaJRXZxc5YmF/WfmxiHX6MMZZasRP6RRQkE%3D' (2024-11-05)
      → 'github:NixOS/nixpkgs/cab778239e705082fe97bb4990e0d24c50924c04?narHash=sha256-lgmUyVQL9tSnvvIvBp7x1euhkkCho7n3TMzgjdvgPoU%3D' (2025-08-04)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/c8cd81426f45942bb2906d5ed2fe21d2f19d95b7?narHash=sha256-tzCdyIJj9AjysC3OuKA%2BtMD/kDEDAF9mICPDU7ix0JA%3D' (2025-04-08)
      → 'github:nixos/nixpkgs/b599843bad24621dcaa5ab60dac98f9b0eb1cabe?narHash=sha256-tLdkkC6XnsY9EOZW9TlpesTclELy8W7lL2ClL%2Bnma8o%3D' (2025-09-08)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/815e4121d6a5d504c0f96e5be2dd7f871e4fd99d?narHash=sha256-uhjnlaVTWo5iD3LXics1rp9gaKgDRQj6660%2BgbUU3cE%3D' (2025-04-04)
      → 'github:numtide/treefmt-nix/1aabc6c05ccbcbf4a635fb7a90400e44282f61c4?narHash=sha256-F1oFfV51AE259I85av%2BMAia221XwMHCOtZCMcZLK2Jk%3D' (2025-08-31)
    • Updated input 'treefmt-nix/nixpkgs':
        'github:nixos/nixpkgs/0e82ab234249d8eee3e8c91437802b32c74bb3fd?narHash=sha256-zExSA1i/b%2B1NMRhGGLtNfFGXgLtgo%2BdcuzHzaWA6w3Q%3D' (2024-12-30)
      → 'github:nixos/nixpkgs/cab778239e705082fe97bb4990e0d24c50924c04?narHash=sha256-lgmUyVQL9tSnvvIvBp7x1euhkkCho7n3TMzgjdvgPoU%3D' (2025-08-04)
    
    * fix: pnpm.fetchDeps now requires fetcherVersion
    
    * feat: parse `nodeVersion` and `pnpmVersion` from `package.json`
    
    * feat: use `nixpkgs-unstable` branch for Darwin

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -2,23 +2,24 @@
   toolchain =
     final: prev:
     let
-      # TODO: parse from .npmrc and package.json
-      nodeVersion = "20.19.0";
-      pnpmVersion = "9.15.9";
+      # TODO: prepare prefetch script
+      packageJson = builtins.fromJSON (builtins.readFile ../package.json);
+      nodeVersion = builtins.replaceStrings [ "^" ] [ "" ] packageJson.devEngines.runtime.version;
+      pnpmVersion = builtins.replaceStrings [ "pnpm@" ] [ "" ] packageJson.packageManager;
     in
     {
-      nodejs = prev.nodejs_20.overrideAttrs (oldAttrs: {
+      nodejs = prev.nodejs_22.overrideAttrs (oldAttrs: {
         version = nodeVersion;
         src = prev.fetchurl {
           url = "https://nodejs.org/dist/v${nodeVersion}/node-v${nodeVersion}.tar.xz";
-          hash = "sha256-WsJRb8kFtqC8GjPnMCk36sZkqCC4h8yGvUjANfujktc=";
+          hash = "sha256-Eg4PdEGQl6n6+uH9gLned5Glh+bxxIwisZMjnM0PcIQ=";
         };
       });
       pnpm = prev.pnpm_9.overrideAttrs (oldAttrs: {
         version = pnpmVersion;
         src = prev.fetchurl {
           url = "https://registry.npmjs.org/pnpm/-/pnpm-${pnpmVersion}.tgz";
-          hash = "sha256-z4anrXZEBjldQoam0J1zBxFyCsxtk+nc6ax6xNxKKKc=";
+          hash = "sha256-hMGeeI19fuJI5Ka3FS+Ou6D0/nOApfRDyhfXbAMAUtI=";
         };
       });
     };
