65160a3b6be6043838bc569dfa4d1dea844efb23
Akira Komamura <akira.komamura@gmail.com>
2025-11-27 20:35:17 +0900

    Bump the pnpm version to match the source

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -9,7 +9,7 @@
         version = sources.nodejs.version;
         src = prev.fetchurl sources.nodejs.src;
       });
-      pnpm = prev.pnpm_9.overrideAttrs (oldAttrs: {
+      pnpm = prev.pnpm_10.overrideAttrs (oldAttrs: {
         version = sources.pnpm.version;
         src = prev.fetchurl sources.pnpm.src;
       });
